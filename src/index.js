80958054d0777a5fe255826bce5ae51f247ae5ce
vidushi agarwal <vidushiagarwal@vidushis-MacBook-Pro.local>
2021-06-10 03:05:06 +0530

    added navbar, sign in, and modal box

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./components/navbar";
+import "./styles/App.scss";
+import { BrowserRouter } from "react-router-dom";
+import { AppContainer } from "react-hot-loader";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(NavBar);
+
+if (module.hot) {
+  module.hot.accept("./components/navbar", () => {
+    const NextApp = require("./components/navbar").default;
+    render(NextApp);
+  });
+}
